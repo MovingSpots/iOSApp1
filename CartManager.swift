48c83fa83f38769e6904978fc7d08f637bbde72d
Selvaraj Thyagarajan <Selvaraj.Thyagarajan@triosstudent.com>
2026-06-01 14:46:23 -0400

    Update CartManager.swift

--- CartManager.swift
+++ CartManager.swift
@@ -4,4 +4,23 @@
 //
 //  Created by SELVARAJ THYAGARAJAN on 2026-06-01.
 //
+import Foundation
+import Combine
+
+class CartManager: ObservableObject {
+    
+    @Published var items: [Coffee] = []
+    
+    func add(_ coffee: Coffee) {
+        items.append(coffee)
+    }
+    
+    func remove(_ coffee: Coffee) {
+        items.removeAll { $0.id == coffee.id }
+    }
+    
+    var total: Double {
+        items.reduce(0) { $0 + $1.price }
+    }
+}
 
