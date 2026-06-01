6da2028be29e696be2ec11435a3b9bc9834fb481
Selvaraj Thyagarajan <Selvaraj.Thyagarajan@triosstudent.com>
2026-06-01 15:04:30 -0400

    Update Coffee.swift

--- Model/Coffee.swift
+++ Model/Coffee.swift
@@ -5,3 +5,13 @@
 //  Created by SELVARAJ THYAGARAJAN on 2026-06-01.
 //
 
+import Foundation
+
+struct Coffee: Identifiable, Codable {
+    let id: String
+    let name: String
+    let description: String
+    let price: Double
+    let image: String
+    let category: String
+}
